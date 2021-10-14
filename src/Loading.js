a312e028a05e9a7c4ba0baee70a0c06109cc6edd
seondal <sseondal@gmail.com>
2021-10-14 16:31:44 +0900

    Create Loading.js

---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoadCircle = styled.div``;
+
+const Loading = () => {
+  return <LoadCircle />;
+};
+
+export default Loading;
